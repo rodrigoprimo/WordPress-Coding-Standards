4e79e9ed2132c979c60f2b6098d9fde0cf824d1e
Rodrigo Primo <rodrigosprimo@gmail.com>
2025-08-05 16:27:08 -0300

    PR 2631 WP/GetMetaSingle: simplify code using two class constants
    Now that support for PHP < 5.6 has been dropped, we can simplify the code by defining the two different function formats as class constants. Before PHP 5.6 it was not possible to declare arrays as class constants.

--- WordPress/Sniffs/WP/GetMetaSingleSniff.php
+++ WordPress/Sniffs/WP/GetMetaSingleSniff.php
@@ -33,6 +33,48 @@ final class GetMetaSingleSniff extends AbstractFunctionParameterSniff {
 	 */
 	const METRIC_NAME = 'get_*meta() function called with $single parameter';
 
+    /**
+     * Relevant signature structure for specific meta functions.
+     *
+     * These functions use 'key' as the parameter name at position 2 and
+     * 'single' as the parameter name at position 3.
+     *
+     * @since 3.2.0
+     *
+     * @var array<string, array<string, int|string>>
+     */
+    const SPECIFIC_META_FUNCTIONS_FORMAT = array(
+        'condition'   => array(
+            'param_name' => 'key',
+            'position'   => 2,
+        ),
+        'recommended' => array(
+            'param_name' => 'single',
+            'position'   => 3,
+        ),
+    );
+
+    /**
+     * Relevant signature structure for generic meta functions.
+     *
+     * These functions use 'meta_key' as the parameter name at position 3 and
+     * 'single' as the parameter name at position 4.
+     *
+     * @since 3.2.0
+     *
+     * @var array<string, array<string, int|string>>
+     */
+    const GENERIC_META_FUNCTIONS_FORMAT = array(
+        'condition'   => array(
+            'param_name' => 'meta_key',
+            'position'   => 3,
+        ),
+        'recommended' => array(
+            'param_name' => 'single',
+            'position'   => 4,
+        ),
+    );
+
 	/**
 	 * Relevant signature structure for generic meta functions.
 	 *
