1d35d7e4b3355989357aa7eced905a38398ea703
Rodrigo Primo <rodrigosprimo@gmail.com>
2025-08-18 15:00:58 -0300

    WIP ContextHelper::is_token_namespaced(): add basic tests
    Check the way the tests are structured. There is for sure room to improve their organization.

---
+++ WordPress/Tests/Helpers/ContextHelper/IsTokenNamespacedUnitTest.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * WordPress Coding Standard.
+ *
+ * @package WPCS\WordPressCodingStandards
+ * @link    https://github.com/WordPress/WordPress-Coding-Standards
+ * @license https://opensource.org/licenses/MIT MIT
+ */
+
+namespace WordPressCS\WordPress\Tests\Helpers\ContextHelper;
+
+use PHPCSUtils\Tokens\Collections;
+use WordPressCS\WordPress\Helpers\ContextHelper;
+use PHPCSUtils\TestUtils\UtilityMethodTestCase;
+
+/**
+ * Tests for the `ContextHelper::is_token_namespaced()` utility method.
+ *
+ * @since 3.3.0
+ *
+ * @covers \WordPressCS\WordPress\Helpers\ContextHelper::is_token_namespaced()
+ */
+final class IsTokenNamespacedUnitTest extends UtilityMethodTestCase {
+
+	/**
+	 * Test is_token_namespaced() returns false if the token does not exist.
+	 *
+	 * @return void
+	 */
+	public function testIsTokenNamespacedReturnFalseIfTokenDoesntExist() {
+		$this->assertFalse( ContextHelper::is_token_namespaced( self::$phpcsFile, -1 ) );
+	}
+
+	/**
+	 * Test is_token_namespaced().
+	 *
+	 * @dataProvider dataIsTokenNamespaced
+	 *
+	 * @param string $testMarker     The comment which prefaces the target token in the test file.
+	 * @param bool   $expectedResult The expected return value.
+	 * @param string $tokenContent   The token content to use for the target token.
+	 *
+	 * @return void
+	 */
+	public function testIsTokenNamespaced( $testMarker, $expectedResult, $tokenContent ) {
+		$stackPtr = $this->getTargetToken( $testMarker, Collections::nameTokens(), $tokenContent );
+		$result   = ContextHelper::is_token_namespaced( self::$phpcsFile, $stackPtr );
+
+		$this->assertSame( $expectedResult, $result, "Failed for: $testMarker" );
+	}
+
+	/**
+	 * Data provider.
+	 *
+	 * @return array<string, array<string, bool|string>>
+	 * @see testIsTokenNamespaced()
+	 */
+	public static function dataIsTokenNamespaced() {
+		return array(
+			'unqualified_function' => array(
+				'testMarker'     => '/* testUnqualifiedFunction */',
+				'expectedResult' => false,
+				'tokenContent'   => 'my_function',
+			),
+			'fully_qualified_function' => array(
+				'testMarker'     => '/* testFullyQualifiedFunction */',
+				'expectedResult' => false,
+				'tokenContent'   => 'my_function',
+			),
+			'unqualified_class' => array(
+				'testMarker'     => '/* testUnqualifiedClass */',
+				'expectedResult' => false,
+				'tokenContent'   => 'MyClass',
+			),
+			'fully_qualified_class' => array(
+				'testMarker'     => '/* testFullyQualifiedClass */',
+				'expectedResult' => false,
+				'tokenContent'   => 'MyClass',
+			),
+			'unqualified_constant' => array(
+				'testMarker'     => '/* testUnqualifiedConstant */',
+				'expectedResult' => false,
+				'tokenContent'   => 'MY_CONSTANT',
+			),
+			'fully_qualified_constant' => array(
+				'testMarker'     => '/* testFullyQualifiedConstant */',
+				'expectedResult' => false,
+				'tokenContent'   => 'MY_CONSTANT',
+			),
+			'partially_qualified_function' => array(
+				'testMarker'     => '/* testPartiallyQualifiedFunction */',
+				'expectedResult' => true,
+				'tokenContent'   => 'my_function',
+			),
+			'fully_qualified_namespaced_function' => array(
+				'testMarker'     => '/* testFullyQualifiedNamespacedFunction */',
+				'expectedResult' => true,
+				'tokenContent'   => 'my_function',
+			),
+			'namespace_relative_function' => array(
+				'testMarker'     => '/* testNamespaceRelativeFunction */',
+				'expectedResult' => true,
+				'tokenContent'   => 'my_function',
+			),
+			'partially_qualified_class' => array(
+				'testMarker'     => '/* testPartiallyQualifiedClass */',
+				'expectedResult' => true,
+				'tokenContent'   => 'MyClass',
+			),
+			'partially_qualified_constant' => array(
+				'testMarker'     => '/* testPartiallyQualifiedConstant */',
+				'expectedResult' => true,
+				'tokenContent'   => 'MY_CONSTANT',
+			),
+		);
+	}
+}
