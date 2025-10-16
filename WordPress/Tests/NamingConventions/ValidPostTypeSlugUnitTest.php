dbe3ec9ec2f52fe9436063241e1769102590d250
Rodrigo Primo <rodrigosprimo@gmail.com>
2025-10-16 13:51:05 -0300

    PR 2633 NamingConventions/ValidPostTypeSlug: add tests for namespaced names

--- WordPress/Tests/NamingConventions/ValidPostTypeSlugUnitTest.php
+++ WordPress/Tests/NamingConventions/ValidPostTypeSlugUnitTest.php
@@ -57,6 +57,7 @@ final class ValidPostTypeSlugUnitTest extends AbstractSniffTestCase {
 					52 => 1,
 					62 => 1,
 					64 => 1,
+					75 => 1,
 				);
 
 			case 'ValidPostTypeSlugUnitTest.2.inc':
